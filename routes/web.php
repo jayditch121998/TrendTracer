0d85995fc8466ace44295462a386b45a3b2cbb2e
Jayditch <jayditch.b@gmail.com>
2025-03-18 16:45:58 +0800

    Implement Front-end React and MUI

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Catch-all route for React SPA
+Route::get('/{any}', function () {
+  return view('app'); // Ensure this points to the correct Blade file
+})->where('any', '.*');
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,49 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+import checker from "vite-plugin-checker";
+import path from "node:path";
+
+const PORT = 3000;
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/js/global.css", "resources/js/main.tsx"],
             refresh: true,
         }),
-        tailwindcss(),
+        react(),
+        checker({
+            typescript: true,
+            eslint: {
+                lintCommand: 'eslint "./resources/js/**/*.{js,jsx,ts,tsx}"',
+                dev: { logLevel: ["error"] },
+            },
+            overlay: {
+                position: "tl",
+                initialIsOpen: false,
+            },
+        }),
     ],
+    resolve: {
+        alias: [
+            {
+                find: /^~(.+)/,
+                replacement: path.join(process.cwd(), "node_modules/$1"),
+            },
+            {
+                find: /^src(.+)/,
+                replacement: path.join(process.cwd(), "resources/js/$1"),
+            },
+        ],
+    },
+    server: {
+      host: "127.0.0.1",  // Forces Vite to use IPv4
+      port: 3000,
+  },
+
+    preview: { port: PORT, host: true },
+    esbuild: {
+      jsx: "automatic", // ✅ Ensures JSX support
+    },
 });
